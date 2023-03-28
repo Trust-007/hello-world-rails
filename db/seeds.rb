a969e0ecc7a2f043445c3964cc4b0ab40f7f0c40
Trust7 <simeontrust7@gmail.com>
2023-03-28 01:19:03 +0100

    Create the table and api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create([{ message: "Hello" }, { message: "Aloha" }, { message: "Hola" }, { message: "Hiiii" }, { message: "What's up" }])
